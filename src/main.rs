1b7af3f766b02b50e710afa74261554cd224c4fb
Platypus88 <79812179+Biped-Potato@users.noreply.github.com>
2024-11-15 22:20:40 -0600

    Final Rough Draft

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,22 @@ const PIXEL_RATIO: f32 = 4.0;
 const GRAVITY: f32 = 2000.;
 const BIRD_ROTATION_RATIO: f32 = 500.;
 const BIRD_FLAP_VELOCITY: f32 = 500.;
-const OBSTACLE_AMOUNT: i32 = 5;
+const OBSTACLE_AMOUNT: i32 = 10;
 const OBSTACLE_SPEED: f32 = 250.0;
 const OBSTACLE_WIDTH: f32 = 32.0;
-const OBSTACLE_RANDOM_VERTICAL_OFFSET: f32 = 50.0;
+const OBSTACLE_RANDOM_VERTICAL_OFFSET: f32 = 40.0;
 const OBSTACLE_GAP_SIZE: f32 = 20.0;
-const OBSTACLE_SPACING: f32 = 400.0;
+const OBSTACLE_SPACING: f32 = 250.0;
 const OBSTACLE_IMAGE_HEIGHT: f32 = 144.0;
 
 #[derive(Component)]
 pub struct Bird {
     pub velocity: f32,
+}
+#[derive(Resource)]
+pub struct GameManager {
+    pub pipe_image: Handle<Image>,
+    pub window_dimensions: Vec2,
 }
 pub fn setup_level(
     asset_server: Res<AssetServer>,
@@ -31,7 +36,12 @@ pub fn setup_level(
     window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
     let window = window_query.get_single().unwrap();
-    commands.insert_resource(ClearColor(Color::srgb(0.01, 0.01, 0.05)));
+    commands.insert_resource(ClearColor(Color::srgb(0.5, 0.7, 0.8)));
+    let pipe_image = asset_server.load("pipe.png");
+    commands.insert_resource(GameManager {
+        pipe_image: pipe_image.clone(),
+        window_dimensions: Vec2::new(window.width(), window.height()),
+    });
     commands.spawn(Camera2dBundle {
         transform: Transform::IDENTITY,
         ..Default::default()
@@ -45,21 +55,28 @@ pub fn setup_level(
         Bird { velocity: 0. },
     ));
     let mut rand = thread_rng();
-
+    spawn_obstacles(&mut commands, &mut rand, window.width(), &pipe_image);
+}
+pub fn spawn_obstacles(
+    mut commands: &mut Commands,
+    mut rand: &mut ThreadRng,
+    window_width: f32,
+    pipe_image: &Handle<Image>,
+) {
     for i in 0..OBSTACLE_AMOUNT {
         let y_offset = generate_random_pipe_offset(&mut rand);
-        let x_position = window.width() / 2. + OBSTACLE_SPACING * i as f32;
+        let x_position = window_width / 2. + OBSTACLE_SPACING * i as f32;
         spawn_obstacle(
             Vec3::X * x_position + Vec3::Y * (get_pipe_centered_position() + y_offset),
             1.,
             &mut commands,
-            &asset_server,
+            pipe_image,
         );
         spawn_obstacle(
             Vec3::X * x_position + Vec3::Y * (-get_pipe_centered_position() + y_offset),
             -1.,
             &mut commands,
-            &asset_server,
+            pipe_image,
         );
     }
 }
@@ -74,7 +91,7 @@ pub fn spawn_obstacle(
     translation: Vec3,
     pipe_direction: f32,
     commands: &mut Commands,
-    asset_server: &Res<AssetServer>,
+    pipe_image: &Handle<Image>,
 ) {
     commands.spawn((
         SpriteBundle {
@@ -83,18 +100,19 @@ pub fn spawn_obstacle(
                 PIXEL_RATIO * -pipe_direction,
                 PIXEL_RATIO,
             )),
-            texture: asset_server.load("pipe.png"),
+            texture: pipe_image.clone(),
             ..Default::default()
         },
         Obstacle { pipe_direction },
     ));
 }
 pub fn update_bird(
+    mut commands: Commands,
+    mut bird_query: Query<(&mut Bird, &mut Transform), Without<Obstacle>>,
+    mut obstacle_query: Query<(&Transform, Entity), With<Obstacle>>,
     time: Res<Time>,
     keys: Res<ButtonInput<KeyCode>>,
-    window_query: Query<&Window, With<PrimaryWindow>>,
-    mut bird_query: Query<(&mut Bird, &mut Transform), Without<Obstacle>>,
-    mut obstacle_query: Query<&mut Transform, With<Obstacle>>,
+    game_manager: Res<GameManager>,
 ) {
     if let Ok((mut bird, mut transform)) = bird_query.get_single_mut() {
         if keys.just_pressed(KeyCode::Space) {
@@ -107,13 +125,12 @@ pub fn update_bird(
             Vec3::Z,
             f32::clamp(bird.velocity / BIRD_ROTATION_RATIO, -90., 90.),
         );
+
         let mut bird_dead = false;
-        let window = window_query.get_single().unwrap();
-        if transform.translation.y <= -window.height() / 2. {
+        if transform.translation.y <= -game_manager.window_dimensions.y / 2. {
             bird_dead = true;
-        } 
-        else {
-            for pipe_transform in obstacle_query.iter() {
+        } else {
+            for (pipe_transform, _entity) in obstacle_query.iter() {
                 if (pipe_transform.translation.y - transform.translation.y).abs()
                     < OBSTACLE_IMAGE_HEIGHT * PIXEL_RATIO / 2.
                     && (pipe_transform.translation.x - transform.translation.x).abs()
@@ -124,12 +141,20 @@ pub fn update_bird(
                 }
             }
         }
+        
         if bird_dead {
             transform.translation = Vec3::ZERO;
             bird.velocity = 0.;
-            for mut pipe_transform in obstacle_query.iter_mut() {
-                pipe_transform.translation.x += OBSTACLE_AMOUNT as f32 * OBSTACLE_SPACING;
+            for (_pipe_transform, entity) in obstacle_query.iter_mut() {
+                commands.entity(entity).despawn();
             }
+            let mut rand = thread_rng();
+            spawn_obstacles(
+                &mut commands,
+                &mut rand,
+                game_manager.window_dimensions.x,
+                &game_manager.pipe_image,
+            );
         }
     }
 }
@@ -140,16 +165,15 @@ pub struct Obstacle {
 }
 pub fn update_obstacles(
     time: Res<Time>,
-    window_query: Query<&Window, With<PrimaryWindow>>,
+    game_manager: Res<GameManager>,
     mut obstacle_query: Query<(&mut Obstacle, &mut Transform)>,
 ) {
-    let window = window_query.get_single().unwrap();
     let mut rand = thread_rng();
     let y_offset = generate_random_pipe_offset(&mut rand);
     for (obstacle, mut transform) in obstacle_query.iter_mut() {
         transform.translation.x -= time.delta_seconds() * OBSTACLE_SPEED;
 
-        if transform.translation.x + OBSTACLE_WIDTH / 2. < -window.width() / 2. {
+        if transform.translation.x + OBSTACLE_WIDTH / 2. < -game_manager.window_dimensions.x / 2. {
             transform.translation.x += OBSTACLE_AMOUNT as f32 * OBSTACLE_SPACING;
             transform.translation.y =
                 get_pipe_centered_position() * obstacle.pipe_direction + y_offset;
