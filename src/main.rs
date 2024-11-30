333ef5ae670d32ac87a153728c426e67d7005240
Platypus88 <79812179+Biped-Potato@users.noreply.github.com>
2024-11-29 19:31:39 -0600

    commit before rewriting

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,19 @@ use rand::{rngs::ThreadRng, thread_rng, Rng};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: String::from("Flappy Bird"),
+                        position: WindowPosition::Centered(MonitorSelection::Primary),
+                        resolution: Vec2::new(512., 512.).into(),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
         .add_systems(Startup, setup_level)
         .add_systems(Update, (update_bird, update_obstacles))
         .run();
@@ -14,11 +26,11 @@ const GRAVITY: f32 = 2000.;
 const BIRD_ROTATION_RATIO: f32 = 500.;
 const BIRD_FLAP_VELOCITY: f32 = 500.;
 const OBSTACLE_AMOUNT: i32 = 10;
-const OBSTACLE_SPEED: f32 = 250.0;
+const OBSTACLE_SPEED: f32 = 150.0;
 const OBSTACLE_WIDTH: f32 = 32.0;
-const OBSTACLE_RANDOM_VERTICAL_OFFSET: f32 = 40.0;
-const OBSTACLE_GAP_SIZE: f32 = 20.0;
-const OBSTACLE_SPACING: f32 = 250.0;
+const OBSTACLE_RANDOM_VERTICAL_OFFSET: f32 = 30.0;
+const OBSTACLE_GAP_SIZE: f32 = 15.0;
+const OBSTACLE_SPACING: f32 = 60.0;
 const OBSTACLE_IMAGE_HEIGHT: f32 = 144.0;
 
 #[derive(Component)]
@@ -36,22 +48,22 @@ pub fn setup_level(
     window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
     let window = window_query.get_single().unwrap();
-    commands.insert_resource(ClearColor(Color::srgb(0.5, 0.7, 0.8)));
     let pipe_image = asset_server.load("pipe.png");
+
+    commands.insert_resource(ClearColor(Color::srgb(0.5, 0.7, 0.8)));
     commands.insert_resource(GameManager {
         pipe_image: pipe_image.clone(),
         window_dimensions: Vec2::new(window.width(), window.height()),
     });
-    commands.spawn(Camera2dBundle {
-        transform: Transform::IDENTITY,
+    commands.spawn(Camera2d {
         ..Default::default()
     });
     commands.spawn((
-        SpriteBundle {
-            transform: Transform::IDENTITY.with_scale(Vec3::splat(PIXEL_RATIO)),
-            texture: asset_server.load("Bird.png"),
+        Sprite {
+            image: asset_server.load("Bird.png"),
             ..Default::default()
         },
+        Transform::IDENTITY.with_scale(Vec3::splat(PIXEL_RATIO)),
         Bird { velocity: 0. },
     ));
     let mut rand = thread_rng();
@@ -65,7 +77,7 @@ pub fn spawn_obstacles(
 ) {
     for i in 0..OBSTACLE_AMOUNT {
         let y_offset = generate_random_pipe_offset(&mut rand);
-        let x_position = window_width / 2. + OBSTACLE_SPACING * i as f32;
+        let x_position = window_width / 2. + OBSTACLE_SPACING * PIXEL_RATIO * i as f32;
         spawn_obstacle(
             Vec3::X * x_position + Vec3::Y * (get_pipe_centered_position() + y_offset),
             1.,
@@ -94,15 +106,15 @@ pub fn spawn_obstacle(
     pipe_image: &Handle<Image>,
 ) {
     commands.spawn((
-        SpriteBundle {
-            transform: Transform::from_translation(translation).with_scale(Vec3::new(
-                PIXEL_RATIO,
-                PIXEL_RATIO * -pipe_direction,
-                PIXEL_RATIO,
-            )),
-            texture: pipe_image.clone(),
+        Sprite {
+            image: pipe_image.clone(),
             ..Default::default()
         },
+        Transform::from_translation(translation).with_scale(Vec3::new(
+            PIXEL_RATIO,
+            PIXEL_RATIO * -pipe_direction,
+            PIXEL_RATIO,
+        )),
         Obstacle { pipe_direction },
     ));
 }
@@ -118,8 +130,8 @@ pub fn update_bird(
         if keys.just_pressed(KeyCode::Space) {
             bird.velocity = BIRD_FLAP_VELOCITY;
         }
-        bird.velocity -= time.delta_seconds() * GRAVITY;
-        transform.translation.y += bird.velocity * time.delta_seconds();
+        bird.velocity -= time.delta_secs() * GRAVITY;
+        transform.translation.y += bird.velocity * time.delta_secs();
 
         transform.rotation = Quat::from_axis_angle(
             Vec3::Z,
@@ -141,7 +153,6 @@ pub fn update_bird(
                 }
             }
         }
-        
         if bird_dead {
             transform.translation = Vec3::ZERO;
             bird.velocity = 0.;
@@ -171,10 +182,12 @@ pub fn update_obstacles(
     let mut rand = thread_rng();
     let y_offset = generate_random_pipe_offset(&mut rand);
     for (obstacle, mut transform) in obstacle_query.iter_mut() {
-        transform.translation.x -= time.delta_seconds() * OBSTACLE_SPEED;
+        transform.translation.x -= time.delta_secs() * OBSTACLE_SPEED;
 
-        if transform.translation.x + OBSTACLE_WIDTH / 2. < -game_manager.window_dimensions.x / 2. {
-            transform.translation.x += OBSTACLE_AMOUNT as f32 * OBSTACLE_SPACING;
+        if transform.translation.x + OBSTACLE_WIDTH * PIXEL_RATIO / 2.
+            < -game_manager.window_dimensions.x / 2.
+        {
+            transform.translation.x += OBSTACLE_AMOUNT as f32 * OBSTACLE_SPACING * PIXEL_RATIO;
             transform.translation.y =
                 get_pipe_centered_position() * obstacle.pipe_direction + y_offset;
         }
